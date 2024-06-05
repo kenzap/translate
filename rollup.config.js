9a5dedb1cd90c671afe58a1a836b37576505fa03
Pavel Lukasenko <pavel@kenzap.com>
2024-06-05 16:34:05 +0200

    Adding EU datacenter support

--- rollup.config.js
+++ rollup.config.js
@@ -11,7 +11,7 @@ import replaceHtmlVars from 'rollup-plugin-replace-html-vars';
 import livereload from 'rollup-plugin-livereload'
 
 // get build mode
-const prodEnv = process.env.BABEL_ENV === "production"
+const prodEnv = process.argv.includes('production')
 const targetFolder = "public/"
 const srcFolder = "src/"
 const absolutePath = (dirPath) => path.resolve(__dirname, dirPath)
@@ -60,10 +60,11 @@ const scriptInputs = scriptFiles.reduce((files, input) => {
 const scriptPaths = Object.keys(scriptInputs)
 const scriptOutputs = scriptPaths.reduce((files, file) => {
 	const inputPath = scriptInputs[file]
-	const parts = inputPath.split("/")
+	const slash = inputPath.indexOf('/') !=-1 ? '/':'\\';
+	const parts = inputPath.split(slash)
 	const pathIndex = parts.indexOf("index.js") - 1
 
-	const outputPath = parts.slice(pathIndex).join("/")
+	const outputPath = parts.slice(pathIndex).join(slash)
 
 	workingFolders.push(parts[parts.length-2]);
 	return { [file]: absolutePath(targetFolder + outputPath), ...files }
@@ -158,7 +159,7 @@ const bundles = scriptPaths.map((key) => {
 		plugins.push(
 			livereload({
 				watch: [ 
-				path.resolve(__dirname, 'public'),
+					path.resolve(__dirname, 'public'),
 				],
 				delay: 500,
 				exts: [ 'html', 'js', 'scss', 'sass', 'css' ]
